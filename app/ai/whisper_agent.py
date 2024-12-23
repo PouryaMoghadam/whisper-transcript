14e1edc463c7cccd69c3ad509b66a769357b2c73
PouryaMoghadam <p.moghadam@msn.com>
2024-12-23 21:06:43 +0330

    add model global controller

--- app/ai/whisper_agent.py
+++ app/ai/whisper_agent.py
@@ -1,3 +1,5 @@
+import threading
+
 import torch
 import whisper
 
@@ -9,6 +11,20 @@ WHISPER_MODEL = Config.WHISPER_MODEL
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Ensure using GPU if available
 cache_dir = Config.CACHE_DIR  # Cache directory for caching models
 
+# Global variable for the model, will be loaded once
+model = None
+model_lock = threading.Lock()
+
+def load_model():
+    global model
+    if model is None:
+        with model_lock:  # Ensure thread safety while loading the model
+            if model is None:  # Double-check if model is still not loaded
+                logger.debug(f"Loading model {WHISPER_MODEL} on device: {device}")
+                model = whisper.load_model(WHISPER_MODEL, device=device)
+                logger.debug(f"Model {WHISPER_MODEL} loaded successfully")
+    return model
+
 def transcribe_with_whisper(audio):
     logger.debug(
         "Starting transcription with Whisper model: %s on device: %s",
@@ -17,12 +33,11 @@ def transcribe_with_whisper(audio):
     )
 
     try:
-        # Load the Whisper model (will automatically use GPU if available)
-        model = whisper.load_model(WHISPER_MODEL, device=device)  # Use Whisper's built-in model loading
-        logger.debug(f"Using model: {WHISPER_MODEL} on device: {device}")
+        # Load the model once and reuse it for all requests
+        processor_model = load_model()
 
         # Transcription in "perform" mode (optimized for speed)
-        result = model.transcribe(audio, language=LANG, fp16=False)  # Disable FP16 for more stable performance
+        result = processor_model.transcribe(audio, language=LANG, fp16=False)  # Disable FP16 for more stable performance
 
         logger.debug("Completed transcription")
         return result['text']
